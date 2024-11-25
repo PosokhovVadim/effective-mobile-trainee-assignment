d1c1f9f2a1427ea62b9165a312569ef11c009b17
PosokhovVadim <posokhovvadyusha@gmail.com>
2024-11-26 03:51:03 +0400

    add logger & run()

--- cmd/main.go
+++ cmd/main.go
@@ -2,28 +2,47 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"os"
 	"songs_lib/config"
+	"songs_lib/pkg/logger"
 
 	"github.com/joho/godotenv"
 	"github.com/kelseyhightower/envconfig"
 )
 
-func main() {
+func run() error {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatalf("Error loading environment: %v\n", err)
+		return fmt.Errorf("error loading environment: %v", err)
 	}
 
 	cfg := config.Config{}
 	if err := envconfig.Process("", &cfg); err != nil {
-		log.Fatalf("Error loading environment: %v\n", err)
+		return fmt.Errorf("error loading environment: %v", err)
 	}
 
 	fmt.Printf("%+v\n", cfg)
-	// init logger
+
+	log, err := logger.SetupLogger(&cfg)
+	if err != nil {
+		return fmt.Errorf("error setup logger: %v", err)
+	}
+
+	log.Info("logger setup success")
+
+	log.Info("Config read success")
+
 	// init app lay
 
 	// run server
 
+	return nil
+
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Errorf("%v\n", err)
+		os.Exit(1)
+	}
 }
